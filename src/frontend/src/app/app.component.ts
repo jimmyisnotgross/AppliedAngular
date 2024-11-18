73aee0898ea2ec714bce68e9df8f269b5974158e
Jimmy Gross <jimmyisnotgross@gmail.com>
2024-11-18 13:58:31 -0500

    Starter changes

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 
 import { WelcomeComponent } from './components/welcome.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   template: `
+    <app-nav-bar></app-nav-bar>
     <main class="container mx-auto">
       <app-welcome />
     </main>
   `,
   styles: [],
-  imports: [WelcomeComponent],
+  imports: [WelcomeComponent, NavBarComponent],
 })
 export class AppComponent {}
