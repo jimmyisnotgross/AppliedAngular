86fa046ff9b67baeb1085dc46dbfaad5af2d3302
Jimmy Gross <jimmyisnotgross@gmail.com>
2024-11-20 14:10:50 -0500

    counter

--- src/frontend/src/app/features/counter/pages/ui/ui.component.ts
+++ src/frontend/src/app/features/counter/pages/ui/ui.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-ui',
-  imports: [],
   template: `
     <div data-testid="counter-feature-ui">
-      <button class="btn btn-primary">-</button>
-      <span data-testid="current">0</span>
-      <button class="btn btn-primary">+</button>
+      <button
+        class="btn btn-primary"
+        (click)="decrement()"
+        [disabled]="isDecrementDisabled()"
+      >
+        -
+      </button>
+      <span data-testid="current">{{ counter() }}</span>
+      <button class="btn btn-primary" (click)="increment()">+</button>
     </div>
     <p>ui works!</p>
   `,
   styles: ``,
 })
-export class UiComponent {}
+export class UiComponent {
+  counter = signal(0);
+
+  isDecrementDisabled = computed(() => this.counter() <= 0);
+
+  increment() {
+    this.counter.update((value) => value + 1);
+  }
+
+  decrement() {
+    this.counter.update((value) => (value > 0 ? value - 1 : 0));
+  }
+}
