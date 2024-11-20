d0324abdaba7d6202de7195b016d7c2941d34b59
Jimmy Gross <jimmyisnotgross@gmail.com>
2024-11-20 14:13:44 -0500

    fizzbuzz

--- src/frontend/src/app/features/counter/pages/ui/ui.component.ts
+++ src/frontend/src/app/features/counter/pages/ui/ui.component.ts
@@ -15,6 +15,7 @@ import { Component, computed, signal } from '@angular/core';
       <button class="btn btn-primary" (click)="increment()">+</button>
     </div>
     <p>ui works!</p>
+    <div data-testid="fizzBuzz">{{ fizzBuzz() }}</div>
   `,
   styles: ``,
 })
@@ -22,6 +23,15 @@ export class UiComponent {
   counter = signal(0);
 
   isDecrementDisabled = computed(() => this.counter() <= 0);
+
+  fizzBuzz = computed(() => {
+    const value = this.counter();
+    if (value === 0) return '';
+    if (value % 3 === 0 && value % 5 === 0) return 'FizzBuzz';
+    if (value % 3 === 0) return 'Fizz';
+    if (value % 5 === 0) return 'Buzz';
+    return '';
+  });
 
   increment() {
     this.counter.update((value) => value + 1);
